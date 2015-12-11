49cce8cf64cc2a30b6f96cabbe84c654520da2c1
Orlandy Ariel <alu0100773408@ull.edu.es>
2015-12-11 01:47:01 +0000

    sobreescrito el metodo to_s -> DocumentoElectronico

--- lib/referencia/doc_electronico.rb
+++ lib/referencia/doc_electronico.rb
@@ -4,5 +4,7 @@ class DocumentoElectronico<ReferenciaBase
         super(a_autores,a_titulo,a_anio)
         @m_edicion,@m_tipo_medio,@m_lugar_publicacion,@m_editor,@m_url,@m_fecha_acceso= a_edicion,a_tipo_medio,a_lugar_publicacion,a_editor,a_url,a_fecha_acceso
     end
-
+    def to_s
+        "#{@m_autor_formato} (#{@m_anio}),#{@m_titulo}(#{@m_edicion}). [#{@m_tipo_medio}]. #{@m_lugar}:#{@m_editor}.Disponible en:#{@m_url} #{@m_fecha_acceso}"
+    end
 end
