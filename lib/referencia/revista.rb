ace64e31e0bc8875b05e225f56526bbc58a2f10d
Orlandy Ariel <alu0100773408@ull.edu.es>
2015-12-11 01:40:58 +0000

    sobreescrito to_s -> Revista

--- lib/referencia/revista.rb
+++ lib/referencia/revista.rb
@@ -4,5 +4,7 @@ class Revista<ReferenciaBase
         super(a_autores,a_titulo,a_anio)
         @m_nombre_revista,@m_volumen,@m_numero, @m_paginas = a_nombre_revista, a_volumen,a_numero, a_paginas
     end
-   
+    def to_s
+         "#{@m_autor_formato}(#{@m_anio}). #{@m_titulo}. #{@m_nombre_revista}, #{@m_volumen}(#{@m_numero}),#{@m_paginas}"
+    end
 end
